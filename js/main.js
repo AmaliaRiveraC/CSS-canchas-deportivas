ea902a65ef16581944d3f783d96327cffa746875
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-04-10 22:31:38 -0500

    enlazando documentos

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var botonFut = document.getElementById("botonFutbol");
+var botonBasket  = document.getElementById("botonBasketbol");
+var botonTennis = document.getElementById("botonTennis");
+
+botonFutbol.addEventListener("click", mostrarCanchaFutbol);
+botonBasketbol.addEventListener("click", mostrarCanchaBasketbol);
+botonTennis.addEventListener("click", mostrarCanchaTennis);
+
+function mostrarCanchaFutbol() {
+	alert("css");
+}
+
+function mostrarCanchaBasketbol() {
+	alert("bienvenido");
+}
+
+function mostrarCanchaTennis() {
+ 	alert("a mi vida");
+}
