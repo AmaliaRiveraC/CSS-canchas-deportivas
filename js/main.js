d69598e51b5a8770535f1547d478d7d2e2d73158
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-04-11 12:33:22 +0000

    javascript aparecen de una en una las canchas

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 var botonFut = document.getElementById("botonFutbol");
 var botonBasket  = document.getElementById("botonBasketbol");
 var botonTennis = document.getElementById("botonTennis");
+var canchaFutbol = document.getElementById("contenedorFutbol");
+//var canchaBasketbol = document.getElementById("contenedorBasketbol");
+var canchaTennis = document.getElementById("contenedorTennis");
 
 botonFutbol.addEventListener("click", mostrarCanchaFutbol);
 botonBasketbol.addEventListener("click", mostrarCanchaBasketbol);
 botonTennis.addEventListener("click", mostrarCanchaTennis);
 
 function mostrarCanchaFutbol() {
-	document.getElementById("contenedorFutbol").style.display = "block";
+	canchaFutbol.style.display = "block";
+    //document.getElementById("contenedorBasketbol").style.display = "none";
+    canchaTennis.style.display = "none";
 }
 
 function mostrarCanchaBasketbol() {
-    document.getElementById("contenedorBasketbol").style.display = "block";
+    /*canchaBasketbol.style.display = "block";
+    canchaFutbol.style.display = "none";
+    canchaTennis.style.display = "none";*/
+    alert("Hola css")
 }
 
-function mosstrarCanchaTennis() {
-    document.getElementById("contenedorTennis").style.display = "block";
+function mostrarCanchaTennis() {
+    canchaTennis.style.display = "block";
+    //document.getElementById("contenedorBasketbol").style.display = "none";
+    canchaFutbol.style.display = "none";
 }
